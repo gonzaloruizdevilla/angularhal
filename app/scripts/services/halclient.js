9ba74453a2d27d0ffa7c08453167f5d68702fac2
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2013-09-18 21:12:13 +0200

    style(halclient): passes almost every jslint rule
    Left unused first parameter of a function whose second parameter is used
    and a circular reference between a constructor and a function that
    produces a "was used before it was defined" jslint warning

--- app/scripts/services/halclient.js
+++ app/scripts/services/halclient.js
@@ -1,87 +1,95 @@
-'use strict';
+/*jslint indent: 2, regexp: true */
+/*global angular*/
+(function (angular) {
 
-angular.module('angularhalApp')
-  .provider('halclient', function () {
-    var $http, $q;
+  'use strict';
 
-    function clone(jsonObject){
-      return JSON.parse(JSON.stringify(jsonObject));
-    }
+  angular.module('angularhalApp')
+    .provider('halclient', function () {
+      var $http, $q;
 
-    function isReserved(prop){
-      return prop === "_links" || prop === "_embedded";
-    }
+      function clone(jsonObject) {
+        return JSON.parse(JSON.stringify(jsonObject));
+      }
 
-    function applyTemplateParams(link, templateParams) {
-      if (link.templated) {
-        return link.href.replace(/\{(.+?)\}/g, function(match, paramId) {
+      function isReserved(prop) {
+        return prop === "_links" || prop === "_embedded";
+      }
+
+      function applyTemplateParams(link, templateParams) {
+        var href;
+        if (link.templated) {
+          href = link.href.replace(/\{(.+?)\}/g, function(match, paramId) {
             return templateParams[paramId];
-        });      
-      } else{
-        return link.href;
-      };
-    }
+          });
+        } else {
+          href = link.href;
+        }
+        return href;
+      }
 
-    function addPayload(linkUrl, payload) {
-      var queryString = '';
-      if (payload) {
-        payload.forEach( function(entry) {
-          queryString += entry.name + '=' + entry.value + '&';
-        });
-        queryString = '?' + queryString.substring(0, queryString.length-1);
-      };
-      return linkUrl + queryString;
-    }
+      function addPayload(linkUrl, payload) {
+        var queryString = '';
+        if (payload) {
+          payload.forEach(function(entry) {
+            queryString += entry.name + '=' + entry.value + '&';
+          });
+          queryString = '?' + queryString.substring(0, queryString.length - 1);
+        }
+        return linkUrl + queryString;
+      }
 
-    function get(link){
-      return function (params){
-        var defer = $q.defer(), linkUrl = link.href;
-        if (params) {
-          linkUrl = applyTemplateParams(link, params.templateParams);
-          linkUrl = addPayload(linkUrl, params.payload);
+      function get(link) {
+        return function (params) {
+          var defer = $q.defer(), linkUrl = link.href;
+          if (params) {
+            linkUrl = applyTemplateParams(link, params.templateParams);
+            linkUrl = addPayload(linkUrl, params.payload);
+          }
+          $http.get(linkUrl).success(function (json) {
+            defer.resolve(new ModelWrapper(json));
+          });
+          return defer.promise;
         };
-        $http.get(linkUrl).success(function (json){
-          defer.resolve(new ModelWrapper(json));
-        })
-        return defer.promise;
-      }
-    }
-
-    function ModelWrapper(data) {
-      var model = clone(data);
-      this.url = function (){
-        return model._links.self.href;
       }
 
-      this.get = function (prop){
-        return isReserved(prop) ? undefined : model[prop];
+      function ModelWrapper(data) {
+        var model = clone(data);
+        this.url = function () {
+          return model._links.self.href;
+        };
+
+        this.get = function (prop) {
+          return isReserved(prop) ? undefined : model[prop];
+        };
+
+        this.links = function () {
+          return clone(model._links);
+        };
+
+        this.link = function (rel) {
+          var link = clone(model._links[rel]);
+          link.get = get(link);
+          return link;
+        };
+
+        this.embedded = function () {
+          return clone(model._embedded);
+        };
       }
 
-      this.links = function () {
-        return clone(model._links);
+      function Client() {
+        this.model = function (modelData) {
+          return new ModelWrapper(modelData);
+        };
       }
 
-      this.link = function (rel){
-        var link = clone(model._links[rel]);
-        link.get = get(link);
-        return link;
-      }
+      // Method for instantiating
+      this.$get = ['$http', '$q', function (_$http_, _$q_) {
+        $http = _$http_;
+        $q = _$q_;
+        return new Client();
+      }];
+    });
 
-      this.embedded = function () {
-        return clone(model._embedded);
-      }
-    }
-
-    function Client() {
-      this.model = function (modelData) {
-        return new ModelWrapper(modelData);
-      }
-    }
-
-    // Method for instantiating
-    this.$get = ['$http', '$q', function (_$http_, _$q_) {
-      $http = _$http_;
-      $q = _$q_;
-      return new Client();
-    }];
-  });
+}(angular));
